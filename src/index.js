a118357a71c71e136b5d77a7531bae6602ad4d20
rdg1029 <wcle3456@gmail.com>
2021-12-13 21:39:35 +0900

    Set default camera pos

--- src/index.js
+++ src/index.js
@@ -136,6 +136,7 @@ class World {
 const clock = new THREE.Clock();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.1, 100);
+camera.position.set(16, 2, 16);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
