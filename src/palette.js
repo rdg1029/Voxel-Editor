74a7b421cce77cd292ded90da5851b677aa7bd5a
Caret <wcle3456@gmail.com>
2022-01-17 13:23:32 +0900

    Update palette.js : Init

---
+++ src/palette.js
@@ -0,0 +1,30 @@
+import { voxelData } from './voxel_data';
+
+class Palette {
+    constructor() {
+        this.component = document.getElementById('palette');
+        this.list = [
+            document.getElementById('c1'),
+            document.getElementById('c2'),
+            document.getElementById('c3'),
+            document.getElementById('c4'),
+            document.getElementById('c5'),
+            document.getElementById('c6'),
+            document.getElementById('c7'),
+            document.getElementById('c8'),
+        ];
+        this.colorBoard = document.getElementById('color-board');
+        for (let i = 1; i < 65; i++) {
+            const color = document.createElement('span');
+            color.style.backgroundColor = voxelData[i];
+            color.style.width = '1em';
+            color.style.height = '1em';
+            this.colorBoard.appendChild(color);
+            if (i % 8 === 0) {
+                this.colorBoard.appendChild(document.createElement('br'));
+            }
+        }
+    }
+}
+
+export {Palette};
