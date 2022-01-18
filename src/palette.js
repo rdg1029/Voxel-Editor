c58104a1465b1d51f56d64e159e10805c6148b99
rdg1029 <wcle3456@gmail.com>
2022-01-18 15:53:08 +0900

    Set color board display

--- src/palette.js
+++ src/palette.js
@@ -5,6 +5,7 @@ class Palette {
         this.component = document.getElementById('palette');
         this.list = document.getElementsByClassName('palette-list');
         this.colorBoard = document.getElementById('color-board');
+
         for (let i = 1; i < 65; i++) {
             const color = document.createElement('span');
             color.style.backgroundColor = voxelData[i];
@@ -12,6 +13,11 @@ class Palette {
             if (i % 8 === 0) {
                 this.colorBoard.appendChild(document.createElement('br'));
             }
+        }
+        for (let i = 0, j = this.list.length; i < j; i++) {
+            this.list[i].addEventListener('click', e => {
+                this.colorBoard.style.display = 'block';
+            });
         }
     }
 }
