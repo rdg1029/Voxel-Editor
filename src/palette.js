e4e7fae3462155bedcc08d5c55370602ae8972a3
rdg1029 <wcle3456@gmail.com>
2022-01-18 17:14:55 +0900

    Update palette.js : Add select()

--- src/palette.js
+++ src/palette.js
@@ -2,9 +2,11 @@ import { voxelData } from './voxel_data';
 
 class Palette {
     constructor() {
+        this.selected = 0;
         this.component = document.getElementById('palette');
         this.list = document.getElementsByClassName('palette-list');
         this.colorBoard = document.getElementById('color-board');
+        this.select(this.selected);
 
         for (let i = 1; i < 65; i++) {
             const color = document.createElement('span');
@@ -16,9 +18,15 @@ class Palette {
         }
         for (let i = 0, j = this.list.length; i < j; i++) {
             this.list[i].addEventListener('click', e => {
+                this.select(i);
                 this.colorBoard.style.display = 'block';
             });
         }
+    }
+    select(index) {
+        this.list[this.selected].style.border = 'none';
+        this.list[index].style.border = 'solid';
+        this.selected = index;
     }
 }
 
