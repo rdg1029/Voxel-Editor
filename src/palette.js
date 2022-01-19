12ac7d7429dd00acef400574725ad113e2f67cb5
rdg1029 <wcle3456@gmail.com>
2022-01-19 11:14:25 +0900

    Update palette.js : Edit class

--- src/palette.js
+++ src/palette.js
@@ -1,19 +1,22 @@
 import { voxelData } from './voxel_data';
 
+let selected = 0;
+const paletteListData = new Uint8Array([60, 29, 31, 36, 42, 55, 1, 8]);
+
 class Palette {
     constructor() {
-        this.selected = 0;
-        this.component = document.getElementById('palette');
         this.list = document.getElementsByClassName('palette-list');
-        this.colorBoard = document.getElementById('color-board');
-        this.select(this.selected);
+        this.colorBoard = document.getElementById('color-board');        
+        this.select(selected);
 
         for (let i = 1; i < 65; i++) {
             const color = document.createElement('span');
             color.style.backgroundColor = voxelData[i];
             color.addEventListener('click', e => {
-                this.list[this.selected].style.backgroundColor = voxelData[i];
+                paletteListData[selected] = i;
+                this.list[selected].style.backgroundColor = voxelData[i];
                 this.colorBoard.style.display = 'none';
+                console.log(paletteListData[selected]);
             });
             this.colorBoard.appendChild(color);
             if (i % 8 === 0) {
@@ -28,9 +31,17 @@ class Palette {
         }
     }
     select(index) {
-        this.list[this.selected].style.border = 'none';
-        this.list[index].style.border = 'solid';
-        this.selected = index;
+        const {list} = this;
+        if (index < 0 || index > 7) {
+            console.error('Out of index');
+            return;
+        }
+        list[selected].style.border = 'none';
+        list[index].style.border = 'solid';
+        selected = index;
+    }
+    getSelectedColorCode() {
+        return paletteListData[selected];
     }
 }
 
