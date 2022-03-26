54e028f88671915a890e1ed7c079a48717b0452e
rdg1029 <wcle3456@gmail.com>
2022-03-26 18:11:25 +0900

    Update world_data.js : Remove class & Add functions

--- src/world_data.js
+++ src/world_data.js
@@ -1,45 +1,24 @@
-class WorldData {
-    constructor() {
-        this.buffer = new ArrayBuffer(45);
-        this.setData(this.buffer);
+let worldData = {
+    spawnPoint: [0, 0, 0],
+};
+
+function getWorldData() {
+    const dataObj = JSON.stringify(worldData);
+    const DATA_LENGTH = dataObj.length;
+    const data = new Uint8Array(DATA_LENGTH);
+    for (let i = 0, j = DATA_LENGTH; i < j; i++) {
+        data[i] = dataObj.charCodeAt(i);
     }
-    setData(buffer) {
-        this.buffer = buffer;
-        this.spawnPoint = new Float32Array(this.buffer, 0, 3); // 12 bytes
-        this.NAME_CHAR_SIZE = new Uint8Array(this.buffer, 44, 1); // 1 bytes
-        if (this.NAME_CHAR_SIZE[0] === 0) {
-            this.name = new Uint8Array(this.buffer, 12, 32); // 32 bytes
-        }
-        else {
-            this.name = new Uint16Array(this.buffer, 12, 16);
-        }
-    }
-    getName() {
-        let str = '';
-        for (let i = 0, j = this.name.length; i < j; i++) {
-            if (this.name[i] === 0) break;
-            str += String.fromCharCode(this.name[i]);
-        }
-        return str;
-    }
-    setName(str) {
-        for (let i = 0, j = str.length; i < j; i++) {
-            if (str.charCodeAt(i) > 255) {
-                this.NAME_CHAR_SIZE[0] = 1;
-                this.name = new Uint16Array(this.buffer, 12, 16);
-                break;
-            }
-        }
-        for (let i = 0, j = str.length; i < j; i++) {
-            this.name[i] = str.charCodeAt(i);
-        }
-    }
-    setSpawnPoint(x, y, z) {
-        this.spawnPoint[0] = x;
-        this.spawnPoint[1] = y;
-        this.spawnPoint[2] = z;
-    }
+    return data;
 }
 
-const worldData = new WorldData();
-export {worldData};
+function setWorldData(uInt8Arr) {
+    let data = "";
+    for (let i = 0, j = uInt8Arr.length; i < j; i++) {
+        data += String.fromCharCode(uInt8Arr[i]);
+    }
+    console.log(data);
+    worldData = JSON.parse(data);
+}
+
+export {worldData, getWorldData, setWorldData};
