72e674bb9b2a4bc4512fc5c320430208b5cf2e8c
rdg1029 <wcle3456@gmail.com>
2022-03-28 14:41:09 +0900

    Update world_data.js : Set conversion value

--- src/world_data.js
+++ src/world_data.js
@@ -2,12 +2,14 @@ let worldData = {
     spawnPoint: [0, 0, 0],
 };
 
+const CONVERSION = 128;
+
 function getWorldData() {
     const dataObj = JSON.stringify(worldData);
     const DATA_LENGTH = dataObj.length;
     const data = new Uint8Array(DATA_LENGTH);
     for (let i = 0, j = DATA_LENGTH; i < j; i++) {
-        data[i] = dataObj.charCodeAt(i);
+        data[i] = dataObj.charCodeAt(i) + CONVERSION;
     }
     return data;
 }
@@ -15,9 +17,8 @@ function getWorldData() {
 function setWorldData(uInt8Arr) {
     let data = "";
     for (let i = 0, j = uInt8Arr.length; i < j; i++) {
-        data += String.fromCharCode(uInt8Arr[i]);
+        data += String.fromCharCode(uInt8Arr[i] - CONVERSION);
     }
-    console.log(data);
     worldData = JSON.parse(data);
 }
 
